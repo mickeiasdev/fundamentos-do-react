6aedbb9c42203580ae451bc9610d4ff8120245eb
Mickeias Coelho <130601846+mickeiasdev@users.noreply.github.com>
2025-05-05 14:26:50 -0300

    Add files via upload

---
+++ DR2-TP1/src/4/index.jsx
@@ -0,0 +1,16 @@
+// Exercício 4: Em uma aplicação React, declare uma variável let
+// para o nome de um usuário e uma variável const para a idade do usuário,
+// e exiba esses valores na página web.
+
+const nome = "Mickeias";
+let idade = 22;
+
+const Exc4 = () => (
+  <div>
+    <h2>
+      E aí, monitor! meu nome é {nome} e eu tenho {idade} anos!
+    </h2>
+  </div>
+);
+
+export default Exc4;
